b3e3647762c3158eff9d2b50ee51c3eec64854f4
PedroHzd <pediaz333@gmail.com>
2023-05-01 20:24:34 -0500

    finished step 4 and 5

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ let getComputerChoice = () => {
     return threeAnswers[randomNumber()];
 }
 
+function winner(playerSelection, computerSelection) {
+    if(playerSelection.toUpperCase() === `ROCK` && computerSelection.toUpperCase() === `PAPER`) {
+        return `You Lose! Computer choose paper.`;
+    } else if(playerSelection.toUpperCase() === `PAPER` && computerSelection.toUpperCase() === `SCISSORS`) {
+        return `You Lose! Computer choose scissors`;
+    } else if(playerSelection.toUpperCase() === `SCISSORS` && computerSelection.toUpperCase() === `ROCK`) {
+        return `You Lose! Computer choose rock`;
+    } else {
+        return `You Won! The computer lost.`
+    }
+}
+
+console.log(winner(`rock`, getComputerChoice()));

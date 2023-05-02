8059723eb14f635ba965ad994f49a69f0c94128c
PedroHzd <pediaz333@gmail.com>
2023-05-01 19:21:34 -0500

    Finished step 3

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-console.log(10+10);
+let threeAnswers = [`Rock`, `Paper`, `Scissors`];
+
+// Gets a random number from 0 - 2 (both are inclusive)
+let randomNumber = () => Math.floor(Math.random() * (2 - 0 + 1) + 0);
+
+let getComputerChoice = () => {
+    return threeAnswers[randomNumber()];
+}
 
